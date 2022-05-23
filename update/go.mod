49761965ddce6339c6e0faeb55a5c87f948f107f
Khushnood Asif <khushnood99@gmail.com>
2022-05-23 18:44:09 +0100

    update

---
+++ update/go.mod
@@ -0,0 +1,5 @@
+module github.com/khushnoodasif/khushnoodasif/update
+
+go 1.14
+
+require github.com/mmcdole/gofeed v1.0.0 // indirect
---
+++ update/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+func makeReadme(filename string) error {
+
+	date := time.Now().Format("2 Jan 2006")
+
+	updated := "<sub>Last updated by magic on " + date + ".</sub>"
+	data := fmt.Sprintf("\n%s\n", updated)
+
+	// Prepare file with a light coating of os
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Bake at n bytes per second until golden brown
+	_, err = io.WriteString(file, data)
+	if err != nil {
+		return err
+	}
+	return file.Sync()
+}
+
+func main() {
+
+	makeReadme("../README.md")
+
+}

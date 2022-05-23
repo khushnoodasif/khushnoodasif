66bad052acab8c9aa53f24621358512ec5ee4f7b
Khushnood Asif <khushnood99@gmail.com>
2022-05-23 18:55:32 +0100

    update

--- update/main.go
+++ update/main.go
@@ -11,8 +11,7 @@ func makeReadme(filename string) error {
 
 	date := time.Now().Format("2 Jan 2006")
 
-	time_new := "<sub>Last updated by magic on " + date + ".</sub>"
-	data := fmt.Sprintf("\n%s\n", time_new)
+	time_new := "<sub>Last updated: " + date + ".</sub>"
 
 	// Prepare file with a light coating of os
 	file, err := os.Create(filename)
@@ -22,7 +21,7 @@ func makeReadme(filename string) error {
 	defer file.Close()
 
 	// Bake at n bytes per second until golden brown
-	_, err = io.WriteString(file, data)
+	_, err = io.WriteString(file, time_new)
 	if err != nil {
 		return err
 	}
